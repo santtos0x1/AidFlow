7b4d6afde3b8f94d7999f999a616ec3357494491
santtos0x1 <ruanrodrigues393@gmail.com>
2025-09-20 13:43:15 -0300

    Beta Search-bar

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,13 +1,24 @@
 from django import forms
-from .models import Ticket, Status
-   
+from .models import Ticket
+
+
 class TicketCreateForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ['title', 'description', 'category', 'priority']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].empty_label = ""
+        self.fields['priority'].empty_label = None
     
 
 class TicketEditForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ['title', 'description', 'category', 'priority', 'status']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['category'].empty_label = ""
+        self.fields['priority'].empty_label = ""
