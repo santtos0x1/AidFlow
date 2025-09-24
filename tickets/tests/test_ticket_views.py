636bb0b262dd52e33dec35e36264b7c9f519282a
santtos0x1 <ruanrodrigues393@gmail.com>
2025-09-24 17:28:22 -0300

    Unit Test: Structural and Functional modifications

--- tickets/tests/test_ticket_views.py
+++ tickets/tests/test_ticket_views.py
@@ -2,6 +2,7 @@ from django import urls
 
 from tickets import views
 from . import test_ticket_base as ttb
+from tickets.models import Ticket
 
 
 class TicketViewsTest(ttb.BaseTicketTest):
@@ -10,79 +11,95 @@ class TicketViewsTest(ttb.BaseTicketTest):
     
     def test_ticket_home_view_function_is_correct(self):
         view_func = urls.resolve(urls.reverse('tickets:home')).func
+        
         self.assertIs(view_func, views.home)
 
     def test_ticket_home_view_returns_status_code_200_ok(self):
         response = self.client.get(urls.reverse('tickets:home'))
+        
         self.assertEqual(response.status_code, 200)
     
     def test_ticket_home_view_returns_status_code_404_not_found_if_no_ticket(self):
         self.ticket.delete()
+        
         response = self.client.get(urls.reverse('tickets:home'))
+        
         self.assertEqual(response.status_code, 404)
         
     def test_ticket_home_view_content_shows_the_correct_value(self):
         response = self.client.get(urls.reverse('tickets:home'))
         content = response.content.decode('utf-8')
+        
         self.assertIn('Test', content)
         self.assertIn('high', content)
     
     def test_ticket_home_view_loads_correct_template(self):
         response = self.client.get(urls.reverse('tickets:home'))
+        
         self.assertTemplateUsed(response, self.templates_paths['home'])
 
     """ ============== Get-Started Testing ============== """
     
     def test_ticket_get_started_view_function_is_correct(self):
         view_func = urls.resolve(urls.reverse('tickets:get-started')).func
+        
         self.assertIs(view_func, views.get_started)
         
     def test_ticket_get_started_view_returns_status_code_200_ok(self):
         response = self.client.get(urls.reverse('tickets:get-started'))
+        
         self.assertEqual(response.status_code, 200)
 
     def test_ticket_get_started_view_loads_correct_template(self):
         response = self.client.get(urls.reverse('tickets:get-started'))
+        
         self.assertTemplateUsed(response, self.templates_paths['get_started'])
 
     """ ============== New Testing ============== """
     
     def test_ticket_new_view_function_is_correct(self):
         view_func = urls.resolve(urls.reverse('tickets:new')).func
+        
         self.assertIs(view_func, views.new_ticket)
 
     def test_ticket_new_view_content_shows_the_correct_value(self):
         response = self.client.get(urls.reverse('tickets:new'))
         content = response.content.decode('utf-8')
+        
         self.assertIn('id_title', content)
         self.assertIn('id_description', content)
 
     def test_ticket_new_view_returns_status_code_200_ok(self):
         response = self.client.get(urls.reverse('tickets:new'))
+        
         self.assertEqual(response.status_code, 200)
 
     def test_ticket_new_view_loads_correct_template(self):
         response = self.client.get(urls.reverse('tickets:new'))
+        
         self.assertTemplateUsed(response, self.templates_paths['new'])
 
     """ ============== Search Testing ============== """
     
     def test_ticket_search_view_function_is_correct(self):
         view_func = urls.resolve(urls.reverse('tickets:search')).func
+        
         self.assertIs(view_func, views.search_ticket)
 
     def test_ticket_search_view_returns_status_code_200_ok(self):
         response = self.client.get(urls.reverse('tickets:search'))
+        
         self.assertEqual(response.status_code, 200)
 
     def test_ticket_search_view_content_shows_the_correct_value(self):
         response = self.client.get(urls.reverse('tickets:search'))
         content = response.content.decode('utf-8')
-        print(content)
+        
         self.assertIn('input-site-search', content)
 
     def test_ticket_search_view_loads_correct_template(self):
         response = self.client.get(urls.reverse('tickets:search'))
+        
         self.assertTemplateUsed(response, self.templates_paths['search'])
 
     """ ============== Detail Testing ============== """
@@ -91,47 +108,63 @@ class TicketViewsTest(ttb.BaseTicketTest):
         view_func = urls.resolve(
             urls.reverse(
                 'tickets:detail',
-                kwargs={'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             )
         ).func
+        
         self.assertIs(view_func, views.details)
 
     def test_ticket_detail_view_returns_status_code_200_ok(self):
         response = self.client.get(
             urls.reverse(
                 'tickets:detail',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             )
         )
+        
         self.assertIs(response.status_code, 200)
         
     def test_ticket_detail_view_content_shows_the_correct_value(self):
         response = self.client.get(
                 urls.reverse('tickets:detail',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             ),
         )
         content = response.content.decode('utf-8')
+        
         self.assertIn('Test', content)
         self.assertIn('high', content)
     
     def test_ticket_detail_view_returns_status_code_404_not_found_if_no_ticket(self):
         self.ticket.delete()
+        
         response = self.client.get(
             urls.reverse(
                 'tickets:detail',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             )
         )
+        
         self.assertEqual(response.status_code, 404)
     
     def test_ticket_detail_view_loads_correct_template(self):
         response = self.client.get(
             urls.reverse(
                 'tickets:detail',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             )
         )
+        
         self.assertTemplateUsed(response, self.templates_paths['details'])
     
     """ ============== Edit Testing ============== """
@@ -140,9 +173,12 @@ class TicketViewsTest(ttb.BaseTicketTest):
         view_func = urls.resolve(
             urls.reverse(
                 'tickets:edit',
-                kwargs={'uuid': self.ticket.uuid}
+                kwargs={
+                    'uuid': self.ticket.uuid
+                }
             )
         ).func
+        
         self.assertIs(view_func, views.edit_ticket)
 
     def test_ticket_edit_view_returns_status_code_200_ok(self):
@@ -162,15 +198,19 @@ class TicketViewsTest(ttb.BaseTicketTest):
             ),
             data=data
         )
+        
         self.assertEqual(response.status_code, 302)
 
     def test_ticket_edit_view_content_shows_the_correct_value(self):
         response = self.client.get(
                 urls.reverse('tickets:edit',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             ),
         )
         content = response.content.decode('utf-8')
+        
         self.assertIn('Test', content)
         self.assertIn('high', content)
 
@@ -184,23 +224,31 @@ class TicketViewsTest(ttb.BaseTicketTest):
             'priority': self.ticket.priority.id,
             'status': self.ticket.status.id
         }
+        
         self.ticket.delete()
+        
         response = self.client.post(
             urls.reverse(
                 'tickets:edit',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             ),
             data=data
         )
+        
         self.assertEqual(response.status_code, 404)
 
     def test_ticket_edit_view_loads_correct_template(self):
         response = self.client.get(
             urls.reverse(
                 'tickets:edit',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             )
         )
+        
         self.assertTemplateUsed(response, self.templates_paths['edit'])
 
     """ ============== Delete Testing ============== """
@@ -209,100 +257,146 @@ class TicketViewsTest(ttb.BaseTicketTest):
         view_func = urls.resolve(
             urls.reverse(
                 'tickets:delete',
-                kwargs={'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             )
         ).func
+        
         self.assertIs(view_func, views.delete_ticket)
 
-    def test_ticket_delete_view_returns_status_code_200_ok(self):
+    def test_ticket_delete_view_returns_status_code_302_redirect(self):
         self.client.login(username=self.user.username, password='123')
     
         response = self.client.post(
             urls.reverse(
-                'tickets:edit',
-                kwargs = {'uuid': self.ticket.uuid}
-            )
+                'tickets:delete',
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
+            ),
+            data = {
+                'action': 'delete_confirm'
+            }
         )
-        self.assertEqual(response.status_code, 200)
+        
+        self.assertEqual(response.status_code, 302)
 
     def test_ticket_delete_view_returns_status_code_404_not_found_if_no_ticket(self):
         self.client.login(username=self.user.username, password='123')
-    
         self.ticket.delete()
+        
         response = self.client.post(
             urls.reverse(
-                'tickets:edit',
-                kwargs = {'uuid': self.ticket.uuid}
-            )
+                'tickets:delete',
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
+            ),
+            data = {
+                'action': 'delete_confirm'
+            }
         )
+        
         self.assertEqual(response.status_code, 404)
 
     def test_ticket_delete_view_loads_correct_template(self):
         response = self.client.get(
             urls.reverse(
                 'tickets:delete',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             )
         )
+        
         self.assertTemplateUsed(response, self.templates_paths['delete'])
+
+    def test_ticket_delete_view_removes_from_database(self):
+        response = self.client.post(
+            urls.reverse(
+                'tickets:delete',
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                },
+            ),
+            data = {
+                'action': 'delete_confirm'
+            }
+        )
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Ticket.objects.filter(uuid=self.ticket.uuid).exists())
 
     """ ============== Reply Testing ============== """ 
     
     def test_ticket_reply_view_returns_status_code_200_ok(self):
         self.client.login(username=self.user.username, password='123')
         
-        data = {
-            'solution': self.ticket.solution
-        }
         response = self.client.post(
             urls.reverse(
                 'tickets:reply',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             ),
-            data=data
+            data = {
+                'solution': self.ticket.solution
+            }
         )
+        
         self.assertEqual(response.status_code, 302)
     
     def test_ticket_reply_view_content_shows_the_correct_value(self):
         response = self.client.get(
                 urls.reverse('tickets:reply',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             ),
         )
         content = response.content.decode('utf-8')
+        
         self.assertIn('Test', content)
         
     def test_ticket_reply_view_returns_status_code_404_not_found_if_no_ticket(self):
         self.client.login(username=self.user.username, password='123')
-        
-        data = {
-            'solution': self.ticket.solution
-        }
-        
         self.ticket.delete()
+        
         response = self.client.post(
             urls.reverse(
                 'tickets:reply',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             ),
-            data=data
+            data = {
+                'solution': self.ticket.solution
+            }
         )
+        
         self.assertEqual(response.status_code, 404)
     
     def test_ticket_reply_view_loads_correct_template(self):
         response = self.client.get(
             urls.reverse(
                 'tickets:reply',
-                kwargs = {'uuid': self.ticket.uuid}
+                kwargs = {
+                    'uuid': self.ticket.uuid
+                }
             )
         )
+        
         self.assertTemplateUsed(response, self.templates_paths['reply'])
 
     def test_ticket_reply_view_function_is_correct(self):
         view_func = urls.resolve(
             urls.reverse(
                 'tickets:reply',
-                kwargs={'uuid': self.ticket.uuid}
+                kwargs={
+                    'uuid': self.ticket.uuid
+                }
             )
         ).func
+        
         self.assertIs(view_func, views.reply_ticket)
